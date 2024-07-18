13f3f436caedad6a615918b2b654d3e16e5d4bc2
Dragos-Stefan Alexa <drgs_lx@yahoo.com>
2024-07-18 15:03:59 +0300

    finished to add mealSlug

---
+++ app/meals/[mealSlug]/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+
+function mealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={classes.header}>
+        <Link href="/meals">
+          <p className={classes.back}>back</p>
+        </Link>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
+}
+
+export default mealDetailsPage;
